a4bd294f78402116fc12b8c94509466f562efaf7
arjantijms <arjan.tijms@gmail.com>
2013-02-05 18:37:02 +0100

    Re-organized auth code. WIP

--- src/main/java/org/example/kickoff/plumbing/jaspic/KickoffServerAuthModule.java
+++ src/main/java/org/example/kickoff/plumbing/jaspic/KickoffServerAuthModule.java
@@ -4,10 +4,9 @@ import static javax.security.auth.message.AuthStatus.SUCCESS;
 import static org.omnifaces.util.Utils.coalesce;
 
 import java.io.IOException;
+import java.util.List;
 
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.SessionScoped;
-import javax.naming.InitialContext;
+import javax.enterprise.inject.spi.BeanManager;
 import javax.security.auth.Subject;
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
@@ -20,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.example.kickoff.auth.Authenticator;
 import org.example.kickoff.auth.LoginBean;
-import org.example.kickoff.plumbing.cdi.BeanResolver;
+import org.example.kickoff.plumbing.cdi.Beans;
 
 // import org.example.kickoff.cdi.Beans;
 
@@ -35,48 +34,101 @@ public class KickoffServerAuthModule extends HttpServerAuthModule {
 	public AuthStatus validateHttpRequest(HttpServletRequest request, HttpServletResponse response, Subject clientSubject, CallbackHandler handler) {
 		try {
 			
-			InitialContext initialContext = new InitialContext();
-			BeanResolver beanResolver = (BeanResolver) initialContext.lookup("java:global/java-ee-kickoff-app/BeanResolver");
+			Delegators delegators = tryGetDelegators();
 			
-			// "SEVERE: No valid EE environment for injection of org.example.kickoff.auth.Authenticator" if getting reference on
-			// GlassFish.
-			Authenticator authenticator = beanResolver.getInstance(Authenticator.class, SessionScoped.class);
-			
-			LoginBean loginBean = beanResolver.getInstance(LoginBean.class, RequestScoped.class);
-			
-			if (coalesce(authenticator, loginBean) != null && coalesce(loginBean.getLoginUserName(), loginBean.getLoginPassword()) != null) {
-				authenticator.authenticate(loginBean.getLoginUserName(), loginBean.getLoginPassword());
-		
-				// Create a handler (kind of directive) to add the caller principal (AKA
-				// user principal) "test" (=basically user name, or user id)
-				// This will be the name of the principal returned by e.g.
-				// HttpServletRequest#getUserPrincipal
-				CallerPrincipalCallback callerPrincipalCallback = new CallerPrincipalCallback(clientSubject, authenticator.getUserName());
-		
-				// Create a handler to add the group (AKA role) "architect"
-				// This is what e.g. HttpServletRequest#isUserInRole and @RolesAllowed
-				// test for
-				GroupPrincipalCallback groupPrincipalCallback = new GroupPrincipalCallback(clientSubject, (String[]) authenticator.getApplicationRoles().toArray());
-		
-				// Execute the handlers we created above. This will typically add the
-				// "test" principal and the "architect"
-				// role in an application server specific way to the JAAS Subject.
-				try {
-					handler.handle(new Callback[] { callerPrincipalCallback, groupPrincipalCallback });
+			// This SAM is supposed to work following a call to HttpServletRequest#authenticate. Such call is in-context of the component executing it,
+			// which *should* have the correct CDI contexts active to obtain our CDI delegators.
+			//
+			// In case this SAM is triggered at the very beginning of a request (which is before even the first Servlet Filter kicks in), those CDI
+			// contexts are typically not (fully) available.
+			if (delegators != null) {
+				
+				Authenticator authenticator = delegators.getAuthenticator();
+				LoginBean loginBean = delegators.getLoginBean();
+				
+				if (notNull(loginBean.getLoginUserName(), loginBean.getLoginPassword())) {
 					
+					authenticator.authenticate(loginBean.getLoginUserName(), loginBean.getLoginPassword());
+					
+					notifyContainerAboutLogin(request, clientSubject, handler, authenticator.getUserName(), authenticator.getApplicationRoles());
+						
 					return SUCCESS;
-				}
-				catch (IOException | UnsupportedCallbackException e) {
-					e.printStackTrace();
 				}
 				
 			}
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		return AuthStatus.SEND_CONTINUE;
+		return SUCCESS;
+	}
+	
+	private boolean notNull(Object... objects) {
+		return coalesce(objects) != null;
+	}
+	
+	private Delegators tryGetDelegators() {
+
+		try {
+			BeanManager beanManager = Beans.getBeanManager();
+
+			Authenticator authenticator = Beans.getReference(Authenticator.class, beanManager);
+			LoginBean loginBean = Beans.getReference(LoginBean.class, beanManager);
+
+			// Some containers might give us the bean, but then don't allow us the reference it if called
+			// in the "wrong" context.
+			loginBean.getLoginPassword();
+
+			return new Delegators(authenticator, loginBean);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	private static class Delegators {
+
+		private final Authenticator authenticator;
+		private final LoginBean loginBean;
+
+		public Delegators(Authenticator authenticator, LoginBean loginBean) {
+			this.authenticator = authenticator;
+			this.loginBean = loginBean;
+		}
+
+		public Authenticator getAuthenticator() {
+			return authenticator;
+		}
+
+		public LoginBean getLoginBean() {
+			return loginBean;
+		}
+	}
+	
+	private void notifyContainerAboutLogin(HttpServletRequest request, Subject clientSubject, CallbackHandler handler, String userName, List<String> roles) {
+		
+		// Create a handler (kind of directive) to add the caller principal (AKA user principal =basically user name, or user id) that
+		// the authenticator provides.
+		//
+		// This will be the name of the principal returned by e.g. HttpServletRequest#getUserPrincipal
+		CallerPrincipalCallback callerPrincipalCallback = new CallerPrincipalCallback(clientSubject, userName);
+		
+		// Create a handler to add the groups (AKA roles) that the authenticator provides. 
+		//
+		// This is what e.g. HttpServletRequest#isUserInRole and @RolesAllowed for
+		GroupPrincipalCallback groupPrincipalCallback = new GroupPrincipalCallback(clientSubject, (String[]) roles.toArray());
+
+		
+		try {
+			// Execute the handlers we created above. 
+			//
+			// This will typically add the provided principal and roles in an application server specific way to the JAAS Subject.
+			// (it could become entries in a hash table inside the subject, or individual principles, or nested group principles etc.
+			handler.handle(new Callback[] { callerPrincipalCallback, groupPrincipalCallback });
+			
+		} catch (IOException | UnsupportedCallbackException e) {
+			// Should not happen
+			throw new IllegalStateException(e);
+		}
 	}
 
 	
