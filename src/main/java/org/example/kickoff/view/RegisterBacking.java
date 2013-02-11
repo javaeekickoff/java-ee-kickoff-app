ffb09671abd0c0b30b62a53aed61c6f813b917e8
Jan Beernink <jan.beernink@gmail.com>
2013-02-11 12:22:20 +0100

    Added a simple user registration form

---
+++ src/main/java/org/example/kickoff/view/RegisterBacking.java
@@ -0,0 +1,41 @@
+package org.example.kickoff.view;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.example.kickoff.business.UserService;
+import org.example.kickoff.model.User;
+
+@ManagedBean
+@ViewScoped
+public class RegisterBacking {
+
+	@EJB
+	private UserService userService;
+
+	private User user = new User();
+	private String password;
+
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public void register() {
+		userService.registerUser(user, password);
+	}
+}
