3c96aad73ab072084c56e1bdc03d66e202c5569f
Jan Beernink <jan.beernink@gmail.com>
2013-02-20 14:57:13 +0100

    Added support for sorting and filtering to user management page

--- src/main/java/org/example/kickoff/view/admin/UsersBacking.java
+++ src/main/java/org/example/kickoff/view/admin/UsersBacking.java
@@ -1,5 +1,6 @@
 package org.example.kickoff.view.admin;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,12 +8,12 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
 
 import org.example.kickoff.business.UserService;
 import org.example.kickoff.model.Group;
 import org.example.kickoff.model.User;
 import org.primefaces.event.RowEditEvent;
-
 
 @ManagedBean
 @ViewScoped
@@ -23,26 +24,52 @@ public class UsersBacking {
 
 	private List<User> users;
 
+	private List<User> filteredUsers;
+
 	private List<Group> groups;
+
+	private SelectItem[] groupFilterOptions;
 
 	@PostConstruct
 	private void onPreload() {
 		users = userService.getUsers();
 		groups = Arrays.asList(Group.values());
+
+		List<SelectItem> list = new ArrayList<>();
+
+		list.add(new SelectItem("", "Select"));
+		for (Group group : groups) {
+			SelectItem item = new SelectItem(group, group.toString());
+			list.add(item);
+		}
+
+		groupFilterOptions = list.toArray(new SelectItem[list.size()]);
 	}
 
 	public List<User> getUsers() {
 		return users;
 	}
 
+	public List<User> getFilteredUsers() {
+		return filteredUsers;
+	}
+
+	public void setFilteredUsers(List<User> filteredUsers) {
+		this.filteredUsers = filteredUsers;
+	}
+
 	public List<Group> getGroups() {
 		return groups;
 	}
 
-    public void onEdit(RowEditEvent event) {
-    	User user = (User) event.getObject();
+	public SelectItem[] getGroupsFilter() {
+		return groupFilterOptions;
+	}
 
-        userService.update(user);
-    }
+	public void onEdit(RowEditEvent event) {
+		User user = (User) event.getObject();
+
+		userService.update(user);
+	}
 
 }
