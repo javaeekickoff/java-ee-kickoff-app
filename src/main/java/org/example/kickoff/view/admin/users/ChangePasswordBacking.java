b06d2e5fc26441a15318da9f705533baa114fe2e
Jan Beernink <jan.beernink@gmail.com>
2013-02-21 14:03:08 +0100

    Implemented feature to allow administrator users to change any users' password

---
+++ src/main/java/org/example/kickoff/view/admin/users/ChangePasswordBacking.java
@@ -0,0 +1,43 @@
+package org.example.kickoff.view.admin.users;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.validation.constraints.Size;
+
+import org.example.kickoff.business.UserService;
+import org.example.kickoff.model.User;
+
+@ManagedBean
+@ViewScoped
+public class ChangePasswordBacking {
+
+	@EJB
+	private UserService userService;
+
+	private User user;
+
+	@Size(min=8)
+	private String newPassword;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+
+	public void changePassword() {
+		userService.updatePassword(user, newPassword);
+	}
+
+}
