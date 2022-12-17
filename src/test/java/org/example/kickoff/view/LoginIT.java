1a498e84c79b8d23b8452acb78009a679ca3ebae
Arjan Tijms <arjan.tijms@gmail.com>
2022-12-17 12:48:48 +0100

    Update LoginIT to use selenium programmatic API
    Signed-off-by: Arjan Tijms <arjan.tijms@gmail.com>

--- src/test/java/org/example/kickoff/view/LoginIT.java
+++ src/test/java/org/example/kickoff/view/LoginIT.java
@@ -12,10 +12,11 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.archive.importer.MavenImporter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
 
 @RunWith(Arquillian.class)
 public class LoginIT {
@@ -30,33 +31,18 @@ public class LoginIT {
 			.addAsResource("test-persistence.xml", "META-INF/persistence.xml");
 	}
 
-	private WebDriver browser;
+	private WebDriver browser = new ChromeDriver();
 
 	@ArquillianResource
 	private URL baseURL;
 
-	@FindBy(id = "loginForm:email")
-	private WebElement email;
-
-	@FindBy(id = "loginForm:password")
-	private WebElement password;
-
-	@FindBy(id = "loginForm:login")
-	private WebElement login;
-
-	@FindBy(id = "nav-user")
-	private WebElement navUser;
-
-	@FindBy(id = "logoutForm:logout")
-	private WebElement logout;
-
 	@Test
 	public void testLoginAsAdmin() {
 		browser.get(baseURL + "login");
-		email.sendKeys("admin@kickoff.example.org");
-		password.sendKeys("passw0rd");
-		login.click();
+
+		login();
 		assertEquals(baseURL + "admin/users", browser.getCurrentUrl());
+
 		logout();
 	}
 
@@ -64,16 +50,26 @@ public class LoginIT {
 	public void testLoginContinueAsUser() {
 		browser.get(baseURL + "user/profile");
 		assertEquals(baseURL + "login?continue=true", browser.getCurrentUrl());
-		email.sendKeys("user@kickoff.example.org");
-		password.sendKeys("passw0rd");
-		login.click();
+
+		login();
 		assertEquals(baseURL + "user/profile", browser.getCurrentUrl());
+
 		logout();
 	}
 
+	private void login() {
+	    findById("loginForm:email").sendKeys("admin@kickoff.example.org");
+        findById("loginForm:password").sendKeys("passw0rd");
+        findById("loginForm:login").click();
+	}
+
 	private void logout() {
-		new Actions(browser).moveToElement(navUser).click(logout).perform();
+		new Actions(browser).moveToElement(findById("nav-user")).click(findById("logoutForm:logout")).perform();
 		assertEquals(baseURL + "", browser.getCurrentUrl());
+	}
+
+	private WebElement findById(String id) {
+	    return browser.findElement(By.id(id));
 	}
 
 }
